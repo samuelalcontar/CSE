2636d621ee31535a5bcc28a5ebf93c07a73c7618
samuelalcontar <samuelalcontar@gmail.com>
2017-12-05 09:45:04 -0800

    almost done
    12/5/17

--- WarmUps.py
+++ WarmUps.py
@@ -10,3 +10,14 @@
 def Name(first_name,last_name):
     print("%s, %s" % (last_name, first_name))
 
+
+# 12.5.17
+"""Write a function called add_py
+that takes one parameter called "name"
+and prints out name.py
+"""
+
+
+def add_py(name):
+    print("%s.py" % name)
+
