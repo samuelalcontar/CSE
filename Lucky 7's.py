7e2fd6f367805331f2226e9028067b4a77f046c3
samuelalcontar <33761399+samuelalcontar@users.noreply.github.com>
2018-01-10 09:44:50 -0800

    1/10/18
    eyuhthegheu

---
+++ Lucky 7's.py
@@ -0,0 +1,10 @@
+import random
+dice1 = random.randint(1, 6)
+dice2 = random.randint(1, 6)
+print(dice1)
+print(dice2)
+total = dice1 + dice2
+print(total)
+print("You came here with 15 dollars. Lets see what you an do.")
+
+
--- SamuelGuessGame.py
+++
@@ -1,6 +0,0 @@
-
-
-
-print("You guessed it!!, the number is", the_number, "and it only"\
-       " took you", count , "tries")
-
