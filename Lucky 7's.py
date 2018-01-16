a54927d5ac45aa79ac59e62c78131ece7300197d
samuelalcontar <33761399+samuelalcontar@users.noreply.github.com>
2018-01-16 09:28:07 -0800

    1/16/18
    Finished Lucky 7's

--- Lucky 7's.py
+++ Lucky 7's.py
@@ -1,18 +1,23 @@
 import random
-dice1 = random.randint(1, 6)
-dice2 = random.randint(1, 6)
-print("The first dice is %d" % dice1)
-print("The second dice is %d" % dice2)
-total = dice1 + dice2
-print("the amount of dots is %d" % total)
+
 money = 15
-
+plays = 0
 while money > 0:
-
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    print("The first dice is %d" % dice1)
+    print("The second dice is %d" % dice2)
+    total = dice1 + dice2
+    print("the amount of dots is %d" % total)
 
     if total == 7:
         money += 4
+        plays += 1
     else:
         money -= 1
+        plays += 1
     print("the money you have left is %d" % money)
     print (" the total after each roll is %d" % total)
+    print(plays)
+
+
